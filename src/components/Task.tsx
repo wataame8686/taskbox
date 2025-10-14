df34ef00ef5eee6ba63affa0566859aa0f842d0d
Yuichi Saikawa <yuichi.saikawa@gmail.com>
2025-10-15 07:19:19 +0900

    Fix color contrast accessibility violation

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -39,7 +39,6 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-          style={{ backgroundColor: "red" }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
